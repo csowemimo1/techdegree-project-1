b218bfc78a52884fe5772e2443c3ebac87c70690
csowemimo1 <joceboettcher@gmail.com>
2023-10-25 21:43:16 -0400

    add timer, tags and comments

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -15,27 +15,32 @@ const quotes = [
     quote:'In recognizing the humanity of our fellow beings, we pay ourselves the highest tribute.', 
     source: 'Thurgood Marshall',
     citation: 'countryliving.com',
-    year: 1992
+    year: 1992,
+    tags: 'speech, civil rights'
   },
   {
     quote:'Hate is too great a burden to bear. It injures the hater more than it injures the hated.', 
     source: 'Coretta Scott King',
     citation: 'countryliving.com',
+    tags: 'civil rights'
   },
   {
     quote:'My humanity is bound up in yours, for we can only be human together.', 
     source: 'Desmond Tutu',
     citation: 'countryliving.com',
+    tags: 'equality, civil rights'
   },
   {
     quote:'Defining myself, as opposed to being defined by others, is one of the most difficult challenges I face.', 
     source: 'Carol Moseley-Braun',
     citation: 'countryliving.com',
+    tags: 'self-esteem'
   },
   {
     quote:'Success is to be measured not so much by the position that one has reached in life as by the obstacles which he has overcome while trying to succeed.', 
     source: 'Booker T. Washington',
     citation: 'countryliving.com',
+    tags: 'self-esteem'
   }
 ];
 
@@ -49,6 +54,27 @@ function getRandomQuote() {
   return quotes[randomNumber];
 }
 
+/***
+ * `getrandomColor` function returns a number between 0 and 255 to represent the r,b, or g
+***/
+function getrandomColor() {
+  return Math.floor(Math.random() * 256);
+}
+
+/***
+ * `getBackgroundColor` function returns the rgb color
+***/
+function getBackgroundColor() {
+  return `rgb(${getrandomColor()}, ${getrandomColor()}, ${getrandomColor()})`;
+}
+
+/***
+ * `timerToChangeQuote` function sets timer to refresh quotes at 10 seconds
+***/
+function timerToChangeQuote() {
+  setInterval(printQuote, 10000);
+}
+timerToChangeQuote();
 
 /***
  * `printQuote` function gets a random quotes and puts it together for html display
@@ -67,10 +93,14 @@ function printQuote() {
     if(quoteObj.year) {
       html += `<span class="year"> ${quoteObj.year} </span>`;
     }
+    if(quoteObj.tags) {
+      html += `<p><span class="tags">Tags: ${quoteObj.tags} </span></p>`;
+    }
 
     //combine the html
     html += '</p>';
     document.getElementById('quote-box').innerHTML = html; 
+    document.querySelector('body').style.backgroundColor = getBackgroundColor();
 }
 
 
